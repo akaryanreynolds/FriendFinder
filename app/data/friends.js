13e6941fb9abfd8ebb40ea4a000e67fd292d3423
akaryanreynolds <ryanreynolds.ca@gmail.com>
2018-01-15 18:50:29 -0800

    Working Routes
    Need to finish matching logic

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendsArray = [
+    {
+        name: "Ryan",
+          photo: "https://m.media-amazon.com/images/S/aplus-media/vc/0ffc3cff-5aba-4444-9ee9-7f696f889701._SL300__.png",
+          scores: [
+          "5",
+          "1",
+          "4",
+          "4",
+          "5",
+          "1",
+          "2",
+          "5",
+          "4",
+          "1"
+        ]
+    }
+];
+
+module.exports = friendsArray; 
